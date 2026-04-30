1576e6a513ed3cf9f2c18c87812f4f5ed0f06424
AytekinM86 <134071233+AytekinM86@users.noreply.github.com>
2026-04-30 14:12:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -101,4 +101,4 @@ def chat():
         return jsonify({"response": f"Xəta: {str(e)}"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
